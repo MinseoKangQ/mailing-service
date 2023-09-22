db36603f4feb70ea8087f0d17ac4d628f53ddd35
MinseoKangQ <kms02171@naver.com>
2023-09-22 15:00:02 +0900

    User 엔티티 수정

--- src/main/java/com/blending/mailingservice/user/entity/User.java
+++ src/main/java/com/blending/mailingservice/user/entity/User.java
@@ -6,7 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,14 +21,28 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "USER")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long num; // 유저 번호 (PK, 자동 생성)
+
     @Column(name = "user_id")
-    private String id; // 유저 아이디 (PK)
+    private String id; // 유저 아이디
+
+    @Column(name = "user_name")
+    private String name; // 유저 이름
 
     @Column(name = "user_pw")
     private String password; // 유저 비밀번호
+
+    @CreatedDate
+    @Column(name = "user_registered_at", nullable = false, updatable = false)
+    private LocalDateTime registeredAt; // 유저 생성 시간
+
+    @LastModifiedDate
+    @Column(name="user_modified_at", nullable = false)
+    private LocalDateTime modifiedAt = null; // 유저 정보 변경 시간
 
     @OneToMany(mappedBy = "receiver", cascade = CascadeType.ALL)
     private List<Mail> receivedMails = new ArrayList<>(); // 유저가 받은 메일들
