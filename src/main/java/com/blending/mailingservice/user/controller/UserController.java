13d05b2f50fcbe84ddcea9e79ab1f989bafc8f10
MinseoKangQ <kms02171@naver.com>
2023-09-29 17:57:04 +0900

    Add signup and login logic and htmls associated with logics

--- src/main/java/com/blending/mailingservice/user/controller/UserController.java
+++ src/main/java/com/blending/mailingservice/user/controller/UserController.java
@@ -1,27 +1,45 @@
 package com.blending.mailingservice.user.controller;
 
-import com.blending.mailingservice.user.dto.UserSignUpDto;
 import com.blending.mailingservice.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
-@RestController
+@Controller
 @RequestMapping("/mailing-service")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+    // 회원가입 Controller
     @GetMapping("/signup")
-    public String signUp() {
+    public String signUpPage() {
         return "signup";
     }
 
     @PostMapping("/signup")
-    public String signUpSuccess(@RequestBody UserSignUpDto user) {
-        String result = userService.signUp(user);
-        return result;
+    public String signUp(@RequestParam("id") String id,
+                                @RequestParam("name") String name,
+                                @RequestParam("pw") String pw) {
+
+        String result = userService.signUp(id, name, pw);
+        System.out.println(result);
+        return "signup_success";
     }
+
+    // 로그인 Controller
+    @GetMapping("/login")
+    public String loginPage() { return "login"; }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("id") String id,
+                        @RequestParam("pw") String pw) {
+        String result = userService.login(id, pw);
+        System.out.println(result);
+        if(result.equals("login success")) return "login_success";
+        else if(result.equals("pw is incorrect")) return "login_fail_by_pw";
+        else return "login_fail_by_id";
+    }
+
 }
--- src/main/java/com/blending/mailingservice/user/service/UserService.java
+++ src/main/java/com/blending/mailingservice/user/service/UserService.java
@@ -1,6 +1,5 @@
 package com.blending.mailingservice.user.service;
 
-import com.blending.mailingservice.user.dto.UserSignUpDto;
 import com.blending.mailingservice.user.entity.User;
 import com.blending.mailingservice.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +13,10 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public String signUp(UserSignUpDto dto) {
+    public String signUp(String id, String name, String pw) {
 
         // 아이디가 이미 등록되어 있다면
-        Optional<User> existingUser = userRepository.findById(dto.getId());
+        Optional<User> existingUser = userRepository.findById(id);
 
         if (existingUser.isPresent()) {
             return "이미 사용중인 아이디 입니다.";
@@ -25,11 +24,29 @@ public class UserService {
         // 아이디가 등록되어 있지 않다면
         else {
             User newUser = new User();
-            newUser.setId(dto.getId());
-            newUser.setName(dto.getName());
-            newUser.setPassword(dto.getPw());
+            newUser.setId(id);
+            newUser.setName(name);
+            newUser.setPassword(pw);
             userRepository.save(newUser);
             return "유저 생성 성공";
+        }
+
+    }
+
+    public String login(String id, String pw) {
+
+        // 아이디가 존재한다면
+        Optional<User> existingUser = userRepository.findById(id);
+        if(existingUser.isPresent()) {
+            if(existingUser.get().getPassword().equals(pw)) {
+                return "login success";
+            }
+            else {
+                return "pw is incorrect";
+            }
+        }
+        else {
+            return "id is not exists";
         }
 
     }
