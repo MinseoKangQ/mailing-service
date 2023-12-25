773476d6ed02bb60c78226c57d860d236f497289
MinseoKangQ <kms02171@naver.com>
2023-12-26 02:20:17 +0900

    Feat: Change test controller return type from String to ResponstEntity<?>

--- src/main/java/com/blending/mailingservice/test/Test.java
+++ src/main/java/com/blending/mailingservice/test/Test.java
@@ -1,6 +1,7 @@
 package com.blending.mailingservice.test;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/mailing-service")
 public class Test {
 
-    @GetMapping()
-    public String home() {
-        return "mailing-service test";
+    @GetMapping
+    public ResponseEntity<?> home() {
+        return ResponseEntity.status(HttpStatus.OK).body("테스트입니다.");
     }
 }
